8ba5d352260cf00e2698d245287ba1eaf5d6cc8a
Doug Mulley <doug.mulley@market6.com>
2015-04-24 13:52:51 -0500

    Initial commit

---
+++ MK6.GameKeeper.PluginTemplate/Program.cs
@@ -0,0 +1,35 @@
+﻿using Serilog;
+using System;
+using System.Threading;
+
+namespace MK6.GameKeeper.Plugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pluginName = args[0];
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .CreateLogger();
+
+            var sched = new Service();
+            var quitSemaphore = new Semaphore(0, 1);
+
+            Console.CancelKeyPress += delegate
+            {
+                Log.Information("Stopping " + pluginName);
+                sched.Stop();
+                Log.Information(pluginName + " stopped");
+                quitSemaphore.Release();
+            };
+
+            Log.Information("Starting " + pluginName);
+            sched.Start();
+            Log.Information(pluginName + " started");
+
+            quitSemaphore.WaitOne();
+        }
+    }
+}
---
+++ MK6.GameKeeper.PluginTemplate/Service.cs
@@ -0,0 +1,15 @@
+﻿namespace MK6.GameKeeper.Plugin
+{
+    class Service
+    {
+        public void Start()
+        {
+            // Insert logic for starting your service here
+        }
+
+        public void Stop()
+        {
+            // Insert logic for stopping your service here
+        }
+    }
+}
